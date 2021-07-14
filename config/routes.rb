6b318257b739125d881f1054f6022944c6fd1f85
t-nakamura <1am1goodtime@gmail.com>
2021-07-14 10:37:57 +0000

    2-commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.
+  namespace :admin do
+    resources :items, :customers, :genres, :orders
+  end
+  
+  namespace :customers do
+    resources :items, only:[:index, :show]
+    resources :cart_items, :orders, :addresses
+    resources :customers, only:[:show, :update]
+  end
 end
