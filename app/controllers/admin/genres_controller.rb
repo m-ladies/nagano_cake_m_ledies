b52714f4ba135a5592cc9121d228d2676a1102a4
YoshikawaMao <lapinnoir2568stella@gmail.com>
2021-07-17 10:10:35 +0000

    [add]genres/indexページ途中２

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -2,19 +2,27 @@ class Admin::GenresController < ApplicationController
 
   def index
     @genres = Genre.all
-    @genre = Genre.new
+    @genre_new = Genre.new
   end
-  
+
   def create
-    @genre = Genre.
+    @genre = Genre.new(genre_params)
+    # ２. データをデータベースに保存するためのsaveメソッド実行
+    @genre.save
+    # ３. トップ画面へリダイレクト
+    redirect_to admin_genres_path
   end
 
   def edit
   end
-  
+
   def update
   end
-  
 
-  
+  private
+
+  def genre_params
+   params.require(:genre).permit(:name)
+  end
+
 end
