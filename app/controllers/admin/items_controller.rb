822fff8a03a504c30d753d9f895df3a897fbd546
YoshikawaMao <lapinnoir2568stella@gmail.com>
2021-07-18 13:44:48 +0000

    item/create途中

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -17,12 +17,18 @@ class Admin::ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_item_path(@item.id)
   end
 
   def update
   end
 
   private
+  def item_params
+    params.require(:item).permit(:name, :introduction, :price, :image_id, :is_active, :genre_id)
+  end
 
 
 end
