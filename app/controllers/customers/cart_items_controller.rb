2f5180e2b938ba859ba7228bb834697a41a4da5f
mfrkw <xjdk3766@gmail.com>
2021-07-20 03:18:25 +0000

    [カート機能途中]

--- app/controllers/customers/cart_items_controller.rb
+++ app/controllers/customers/cart_items_controller.rb
@@ -2,48 +2,46 @@ class Customers::CartItemsController < ApplicationController
     # before_action :setup_item, only: [:destroy]
      before_action :authenticate_customer!
 
-  
+
   def index
     @cart_items = current_customer.cart_items
-    @sum = 0  #初期設定
   end
-  
+  #商品変更
   def update
     @cart_item = CartItem.find(params[:id])
     @cart_item.update(cart_item_params)
     redirect_to customers_cart_items_path, notice: 'カート内の商品が変更されました'
   end
-	
+  # 商品追加
   def create
-    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
-      @cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+    @cart_item = current_customer.cart_items.new(cart_item_params)
+    @cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+    @cart_item = CartItem.new(cart_item_params)
+    if @cart_item.present?
       @cart_item.amount += params[:cart_item][:amount].to_i
-      @cart_item.save
-      redirect_to cart_items_path, notice: '変更しました'
-    else
-      @cart_item = CartItem.new(cart_item_params)
-      @cart_item.save
-      redirect_to cart_items_path
+      @cart_item.save!
+      redirect_to customers_cart_items_path, notice: 'カート内に商品が追加されました'
     end
-  end
-  
+  end  
+    
+  # 商品を１つ削除
   def destroy
     @cart_item = CartItem.find(params[:id])
 		@cart_item.destroy
-    redirect_to cart_items_path, notice: '削除しました'
+    redirect_to customers_cart_items_path, notice: '削除しました'
   end
 
   def all_destroy
     @cart_items = current_customer.cart_items
     @cart_items.all_destroy
-    redirect_to cart_items_path, notice: 'カートを空にしました'
+    redirect_to customers_cart_items_path, notice: 'カートを空にしました'
   end
 
   private
   def cart_item_params
-    params.require(:cart_item).permit(:amount, :item_id, :customer_id)
+    params.require(:cart_item).permit(:amount, :item_id)
   end
 
-  
+
 
 end
