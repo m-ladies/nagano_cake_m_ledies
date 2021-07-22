fde8be8f91f2f5dae92bf75f0ba6359d072b0999
Hazuki083 <ha882ki@gmail.com>
2021-07-22 07:44:27 +0000

    Updateステータス

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -5,19 +5,24 @@ class Admin::OrdersController < ApplicationController
     @order = Order.find(params[:id])
     @order_details = @order.order_details.all
   end
-  
+
   def update
     
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
+    @order.update(order_params) 
+    if @order.order_status == "入金確認"
+       @order_details.each do |order_detail|
+        order_detail.making_status = "製作待ち"
+        order_detail.save
+      end
+    end
+    redirect_to admin_order_path
   end
 
-
  private
+
 	def order_params
 	  params.require(:order).permit(:order_status)
 	end
-	
-	def order_detail_params
-	  params.require(:order_detail).permit(:making_status)
-  end
-  
 end
