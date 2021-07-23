38907ffe3344548395f09f1bfbb72b45ec934747
Hazuki083 <ha882ki@gmail.com>
2021-07-23 07:56:52 +0000

    フラッシュメッセージ実装

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,5 +1,7 @@
 class Admin::OrderDetailsController < ApplicationController
 
+ before_action :authenticate_admin!
+
  def update
     @order_detail = OrderDetail.find(params[:id])
     @order = @order_detail.order
@@ -15,11 +17,11 @@ class Admin::OrderDetailsController < ApplicationController
      end
     redirect_to admin_order_path(@order_detail.order.id)
  end
- 
+
  private
- 
+
  def order_detail_params
    params.require(:order_detail).permit(:making_status)
  end
- 
+
 end
