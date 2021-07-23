9fff4c27cecc388eb8028f043fb65623811a4a04
YoshikawaMao <lapinnoir2568stella@gmail.com>
2021-07-23 10:17:48 +0000

    logo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
    before_action :configure_permitted_parameters, if: :devise_controller?
-   
+    before_action :authenticate_customer!,except: [:new, :about, :index]
+    # before_action :authenticate_admin!
+
    protected
    # 新規登録の保存機能
     def configure_permitted_parameters
@@ -8,5 +10,5 @@ class ApplicationController < ActionController::Base
   			 keys: [:first_name, :last_name, :first_name_kana, :last_name_kana,
                 :email, :postal_code, :address, :telephone_number])
     end
-  
+
 end
